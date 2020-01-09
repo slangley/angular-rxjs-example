5410ece2b91b69daa7ddd9bdc5bbd9d64f2302a8
slangley <slangley@popcornlabs.com>
2020-01-09 15:00:33 -0500

    2 files modified

--- src/app/controller.service.ts
+++ src/app/controller.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Subject} from 'rxjs';
+import { Subject, Observable, BehaviorSubject } from 'rxjs';
+import { debounceTime, switchMap, distinctUntilChanged} from 'rxjs/operators';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControllerService {
 
-  
-
   public userId = new Subject<string>()
+  public data: BehaviorSubject<any> = new BehaviorSubject<any>([]]);
 
-  constructor() {
-    this.userId.subscribe((x)=> {console.log("CONTROLLER SERVICE", x)});
+
+  constructor(public dataService: DataService) {
+    this.userId.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap((val)=>{
+          return this.dataService.getPosts(val);
+      })
+    ).subscribe(this.data);
+
   }
 
 
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,6 +4,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface Post {
+  id: number,
+  title: string,
+  body: string,
+  userid: number
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +18,11 @@ export class DataService {
 
   constructor( protected httpClient: HttpClient) { }
 
-  getPosts() {
-    return this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
+  getPosts(id: string | null): Observable<Post[]> {
+    let url = 'https://jsonplaceholder.typicode.com/posts'
+    if (id) {
+      url += `/${id}`;
+    }
+    return this.httpClient.get<Post[]>(url);
   }
 }
