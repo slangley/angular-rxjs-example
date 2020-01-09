0d8e45011750e9f7bbbfbdda2d30f818bdf0c930
slangley <slangley@popcornlabs.com>
2020-01-09 11:41:16 -0500

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ControllerService } from './controller.service';
 
 @Component({
   selector: 'hello',
@@ -7,4 +8,6 @@ import { Component, Input } from '@angular/core';
 })
 export class HelloComponent  {
   @Input() name: string;
+
+  constructor( public controller: ControllerService) {}
 }
